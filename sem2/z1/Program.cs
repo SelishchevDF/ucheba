e08ac22a58af7d962f30a4a507e1002f8d979e86
«SelishchevD» <lubimkin2526@mail.ru>
2022-06-29 18:51:45 +0300

    sem2\z1 - ok

---
+++ sem2/z1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Тут писать 
+            /*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+            456 -> 5
+            782 -> 8
+            918 -> 1*/
+            Console.WriteLine("Введите число");
+            string A = Console.ReadLine();
+
+            Console.WriteLine($"второй символ числа {A} - {A[1]}");
+
+            
+        }
+    }
+}
