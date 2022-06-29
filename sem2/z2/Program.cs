9c82de438a516403760f0848076a7cee1e5a68d3
«SelishchevD» <lubimkin2526@mail.ru>
2022-06-29 19:02:51 +0300

    sem2\z2 - ok

---
+++ sem2/z2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Тут писать
+            /*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+            645 -> 5
+            78 -> третьей цифры нет
+            32679 -> 6 */
+            Console.WriteLine("давай число");
+            string A = Console.ReadLine();
+
+            if (A.Length < 3)
+            {
+                Console.WriteLine("третьего символа нема(((");
+            }
+            else 
+            {
+                Console.WriteLine($"третья цифра числа {A} - {A[2]}");
+            }
+        }
+    }
+}
