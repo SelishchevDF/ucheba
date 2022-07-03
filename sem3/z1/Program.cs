2757aa9561d9aabc46207f36b5cd866eb05b0c80
«SelishchevD» <lubimkin2526@mail.ru>
2022-07-03 09:30:28 +0300

    sem3\z1 - ok

--- sem3/z1/Program.cs
+++ sem3/z1/Program.cs
@@ -10,7 +10,39 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Тут писать
-            
-        }
+            Console.WriteLine("введи число");
+            string str = Console.ReadLine();
+
+            // проверка на пятизначность, если нужна
+            //if (str.Length == 5) 
+            //{
+                //метод счета пар для дальнейшей проверки на палиндромность
+                static int shchetpar (string strmetod)
+                {
+                int count = 0;
+                for ( int i = 0; i < strmetod.Length; i++ )
+                {
+                    if (strmetod[i] == strmetod[(strmetod.Length-1)-i])
+                    {
+                        count++;
+                    }
+                }
+                return count;
+                }                 
+                if (str.Length == shchetpar(str))
+                {
+                    Console.WriteLine("У вас палиндромность");
+                }
+                else 
+                {
+                    Console.WriteLine("У вас не палиндром");
+                }
+                            
+            //вторя часть проверки на 5тизначность
+            //else
+            //{
+                //Console.WriteLine("попробуй еще раз ввести пятизначное число");
+            //}
+        }   
     }
 }
