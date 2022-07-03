200da4cc2f4bb013c991d47c5f08a14f3e9749bd
«SelishchevD» <lubimkin2526@mail.ru>
2022-07-03 10:44:08 +0300

    sem3\z2 - ok

--- sem3/z2/Program.cs
+++ sem3/z2/Program.cs
@@ -11,7 +11,29 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Тут писать
-            
+        // метод заполнение точки
+        static int[] tochka()
+        {
+            //магические тройки тк у точки 3 координаты
+            int [] tochka = new int[3];
+            for (int i=0; i < 3; i++)
+            {
+                tochka[i] = new Random().Next(-10,11);
+            }
+            return tochka;
+        }
+        //вывод координат
+        int [] A = tochka();
+        Console.WriteLine($"точка A координаты x={A[0]} y={A[1]} z={A[2]}");
+        int [] B = tochka();
+        Console.WriteLine($"точка B координаты x={B[0]} y={B[1]} z={B[2]}");
+        // метод расчета отрезка
+        static double dlina (int [] tochka1,int [] tochka2)
+        {
+            double dlinaot = Math.Sqrt(Math.Pow(tochka2[0]-tochka1[0],2) + Math.Pow(tochka2[1]-tochka1[1],2) + Math.Pow(tochka2[2]-tochka1[2],2));
+            return dlinaot;
+        }
+        Console.WriteLine($"длина отрезка {dlina(A,B)}");      
         }
     }
 }
