7d3ad2b1e8881fa5b8187e2e49587bc999ef57d5
«Selishchev» <lubimkin2526@mail.ru>
2022-07-06 14:24:00 +0300

    sem4\z2 - ok + osnova

--- sem4/z2/Program.cs
+++ sem4/z2/Program.cs
@@ -14,9 +14,37 @@ namespace GeekBrains
         static void Main(string[] args)
         {
             // Тут писать главное
-        C
+        summ();
             
         }
-    // тут методы
+        // тут методы
+        public static void summ()
+        {
+        //ввод числа
+        Console.WriteLine("Введите число");
+        string str = Console.ReadLine();
+        //обьявление массива для хранения и преобразования числа
+        char [] a = str.ToCharArray(); 
+        //переменная для подсчета суммы
+        int summa = 0;
+        //если число отрицетельное, вместо минуса ткнется 0
+        if (a[0] == '-')
+            a[0] = '0';
+        //проверка на число и нахождеение суммы чисел массива
+        for (int i = 0; i <str.Length; i ++)
+        {
+            if (a[i] == '1' | a[i] == '2' | a[i] == '3' | a[i] == '4' | a[i] == '5' | a[i] == '6' | a[i] == '7' | a[i] == '8' | a[i] == '9' | a[i] == '0')
+            {
+
+                summa = summa + Convert.ToInt32(a[i].ToString());
+            }
+            else
+            {
+                Console.WriteLine("это число с буквами");
+                return;
+            }
+        }
+        Console.WriteLine($"сумма чисел в числе {str} = {summa}");
+        }
     }
 }
