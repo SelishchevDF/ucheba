71ba8cd50b0b98f0e022bf85f9df22649f5343c0
SelishchevDF <107745932+SelishchevDF@users.noreply.github.com>
2024-02-23 11:13:49 +0300

    Add files via upload

---
+++ java/spring/lesson2/src/main/java/l2/lesson2/Lesson2Application.java
@@ -0,0 +1,24 @@
+package l2.lesson2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController
+public class Lesson2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson2Application.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "User") String name) {
+		return String.format("Hello, %s!", name);
+	}
+}
+
+
