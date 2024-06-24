4a502199cc21b811aacccb93905cffbc7b50708a
SelishchevDF <107745932+SelishchevDF@users.noreply.github.com>
2024-06-24 19:06:33 +0300

    Add files via upload

---
+++ javaScript/js2/t5/data.js
@@ -0,0 +1,85 @@
+let data = `
+[
+    {
+      "img": true,
+      "age": 28,
+      "name": "Reed Vang",
+      "gender": "male",
+      "friends": [
+        {
+          "name": "Fulton Berger"
+        },
+        {
+          "name": "Leach Donovan"
+        },
+        {
+          "name": "Donovan Williams"
+        }
+      ]
+    },
+    {
+      "age": 34,
+      "name": "Hanson Mays",
+      "gender": "male",
+      "friends": [
+        {
+          "name": "Elizabeth Manning"
+        },
+        {
+          "name": "Terra Slater"
+        },
+        {
+          "name": "Lourdes Acevedo"
+        }
+      ]
+    },
+    {
+      "age": 33,
+      "name": "Kristina Wynn",
+      "gender": "female",
+      "friends": [
+        {
+          "name": "Compton Casey"
+        },
+        {
+          "name": "Chris Whitley"
+        },
+        {
+          "name": "Melisa Woodard"
+        }
+      ]
+    },
+    {
+      "img": true,
+      "age": 35,
+      "name": "Gray Hunt",
+      "gender": "male",
+      "friends": [
+        {
+          "name": "Juliana Dudley"
+        },
+        {
+          "name": "Valencia Hoffman"
+        },
+        {
+          "name": "Trisha Fuentes"
+        }
+      ]
+    },
+    {
+      "age": 32,
+      "name": "Briana Head",
+      "gender": "female",
+      "friends": [
+        {
+          "name": "Jessica Gregory"
+        },
+        {
+          "name": "Herring Rosario"
+        },
+        {
+          "name": "Virginia Vazquez"
+        }
+      ]
+    }
+  ]`
