35a111019ffaee475318a77fbfa8d8f1c66e9235
SelishchevDF <107745932+SelishchevDF@users.noreply.github.com>
2024-07-02 11:01:49 +0300

    Add files via upload

---
+++ javaScript/js2/t7/data.js
@@ -0,0 +1,35 @@
+let prdsJSON = `[
+    {
+        "image" : [
+            {
+                "src": "images1.jpg",
+                "alt": "Image 1"
+            }
+        ],
+        "title" : "Title 1",
+        "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$300"
+    },
+    {
+        "image" : [
+            {
+                "src": "images2.jpg",
+                "alt": "Image 2"
+            }
+        ],
+        "title" : "ELLERY X M'O CAPSULE",
+        "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$200"
+    },
+    {
+        "image" : [
+            {
+                "src": "images3.jpg",
+                "alt": "Image 3"
+            }
+        ],
+        "title" : "Title 3",
+        "description" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price" : "$100"
+    }
+]`; 
